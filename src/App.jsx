37aaa544d78f714c86af86002ca533c2099133c8
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-06-29 21:23:51 +0500

    React class 01 full intro

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Button from "./Button/Button.jsx";
+
+function App() {
+  // const user = {
+  //   color: "red",
+  //   background: "green"
+  // }
+
+  let username = "Abdullah";
+
+  return (
+    <div>
+      <h1 style={user}>UserName {username}</h1>
+      <Button />
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/Button/Button.jsx
@@ -0,0 +1,8 @@
+import style from './Button.css'
+
+const  Button =()=>{
+    return(
+        <button className={style.btn}>Search</button>
+    )
+}
+export default Button 
